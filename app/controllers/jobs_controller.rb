91667711ac0ddfab6dac759cf2c568d904bb4b85
somedayrainbows <ebassity@gmail.com>
2017-04-02 00:01:51 -0600

    finished crud on jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,15 +25,23 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to company_jobs_path
+    else
+      render :show
+    end
   end
 
   def destroy
-    # implement on your own!
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to company_jobs_path
   end
 
   private
